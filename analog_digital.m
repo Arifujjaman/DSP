c060003107d46db3c8553cedca5209ecd23fdef5
Arifujjaman <37370324+Arifujjaman@users.noreply.github.com>
2019-09-17 22:08:08 +0600

    complete

---
+++ analog_digital.m
@@ -0,0 +1,16 @@
+clc;
+clear all;
+close all;
+%x(t)=sin(2*pi*1000*t)
+N=8;
+F=1000;
+Fs=8000;
+for i=0:N-1
+  x(i+1)=sin(2*pi*(F/Fs)*i);
+endfor
+t=0:N-1;
+subplot(221);
+stem(t,x);
+xlebel('time');
+ylebel('amplitude');
+
