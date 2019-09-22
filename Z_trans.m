800ef5774ff7050acaeb72949e0ac618ccf55399
Arifujjaman <37370324+Arifujjaman@users.noreply.github.com>
2019-09-22 21:12:57 +0600

    com

---
+++ Z_trans.m
@@ -0,0 +1,11 @@
+%sym 'z';
+x = input('Enter the sequence x(n): ');
+n1 = input('Enter the start value of n: ');
+n2 = n1+length(x)-1;
+m = 1;
+result = 0;
+for i=n1:n2
+  result = result + x(m)*(z^(-i));
+  m = m + 1;
+endfor
+disp(result);
